2870fe503f228403d382e73d1b7d6abbf86e92e2
AmilaMirvic <amilamirvic.267@gmail.com>
2023-10-26 22:34:16 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+ <Router>
+  <Header />
+  <Routes> 
+    
+  <Route path="/" element={<Login />} >
+
+
+ </Route>
+  </Routes>
+ 
+
+ </Router>
+    </div>
+  );
+}
+
+export default App;
